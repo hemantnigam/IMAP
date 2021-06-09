39583b11a56c7e6f4f73fc75bede3b3ccdfc2e08
Hemant Nigam <hemantni@ciena.com>
2021-06-09 09:34:49 +0530

    bar chart added

--- src/components/GridTable.js
+++ src/components/GridTable.js
@@ -1,23 +1,11 @@
 import React from "react";
-import { AgGridColumn, AgGridReact } from "ag-grid-react";
-import "ag-grid-community/dist/styles/ag-grid.css";
+import {Table} from "antd";
 
-function GridTable({ rowData }) {
+function GridTable({ rowData, columnsData }) {
   return (
     <>
       <div className="grid-table__header">Top Customers</div>
-      <AgGridReact
-        className="grid-table__content"
-        rowData={rowData}
-        domLayout="autoHeight"
-      >
-        {rowData &&
-          rowData[0] &&
-          Object.keys(rowData[0]).length > 0 &&
-          Object.keys(rowData[0]).map((col, index) => (
-            <AgGridColumn field={col} key={index}></AgGridColumn>
-          ))}
-      </AgGridReact>
+      <Table className="grid-table__content" dataSource={rowData} columns={columnsData} />;
     </>
   );
 }
